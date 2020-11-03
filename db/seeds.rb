7ca47e12f3b32a90ca77aea3a226c3bf1a228eb9
Romain Borremans <romain.borremans@gmail.com>
2020-11-03 15:08:50 +0100

    created model, seed and controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do 
+    Article.create do |article| 
+        article.title = Faker::Lorem.sentence 
+        article.content = Faker::Lorem.paragraph 
+    end 
+end
